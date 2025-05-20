64d43f5f5a6ed4e2062792ac0997d6660e83c30b
Shivam Sinha <shivamsinha2000@gmail.com>
2025-05-20 02:01:16 -0400

    Fix Flask metrics endpoint

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
+from flask import Flask, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+
+c = Counter('my_requests_total', 'Total HTTP Requests')
+
+@app.route('/')
+def hello():
+    c.inc()
+    return "Hello, World!"
 
 @app.route('/metrics')
-def home():
-    return "Flask app running!"
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
